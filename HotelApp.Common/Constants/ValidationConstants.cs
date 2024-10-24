964f0d6f9851a55aad2403cf1206ff413e241dfd
Vasil Delchev <vasildelchev97@gmail.com>
2024-10-24 17:28:13 +0300

    Added validation constants

--- HotelApp.Common/Constants/ValidationConstants.cs
+++ HotelApp.Common/Constants/ValidationConstants.cs
@@ -3,20 +3,24 @@
     public static class ValidationConstants
     {
         // Reservation validation constants
-        public const decimal TotalPriceMinValue = 0m;
-        public const decimal TotalPriceMaxValue = 10_000m;
+        public const decimal ReservationTotalPriceMinValue = 0m;
+        public const decimal ReservationTotalPriceMaxValue = 50_000m;
 
         // Guest validation constants
         public const int GuestFullNameMinLength = 5;
         public const int GuestFullNameMaxLength = 55;
 
-        // Common validation constants
-        public const int RequiredPrecision = 18;
-        public const int RequiredScale = 2;
-
         // Facility validation constants
         public const int FacilityDescriptionMinLength = 5;
         public const int FacilityDescriptionMaxLength = 520;
+
+        // Review validation constants
+        public const int ReviewCommentMinLength = 2;
+        public const int ReviewCommentMaxLength = 1025;
+
+        // Common validation constants
+        public const int RequiredPrecision = 18;
+        public const int RequiredScale = 2;
 
         public const string PhoneNumberPattern = @"^\+[\d]{8,15}$";
         public const string EmailAddressPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
