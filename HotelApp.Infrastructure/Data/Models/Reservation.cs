1c5c3a72d557b8aca833f1cc22e46941dae1d56d
Vasil Delchev <vasildelchev97@gmail.com>
2024-10-24 16:26:51 +0300

    Added comments to Reservation

--- HotelApp.Infrastructure/Data/Models/Reservation.cs
+++ HotelApp.Infrastructure/Data/Models/Reservation.cs
@@ -1,29 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HotelApp.Infrastructure.Data.Models
 {
+    [Comment("Reservation in the Hotel")]
     public class Reservation
     {
         [Key]
+        [Comment("Identifier")]
         public int Id { get; set; }
 
         [Required]
+        [Comment("Shows the total price after any additional facilities, if there is any")]
         public decimal TotalPrice { get; set; }
 
         [Required]
+        [Comment("Reservation start day")]
         public DateTime DateStart { get; set; }
 
         [Required]
+        [Comment("Reservation end date")]
         public DateTime DateEnd { get; set; }
 
         [Required]
+        [Comment("Identity user identifier")]
         public string ApplicationUserId { get; set; } = string.Empty;
         [Required]
         [ForeignKey(nameof(ApplicationUserId))]
         public ApplicationUser ApplicationUser { get; set; } = null!;
 
         [Required]
+        [Comment("Room identifier")]
         public int RoomId { get; set; }
         [Required]
         [ForeignKey(nameof(RoomId))]
