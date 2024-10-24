dc66d1d6219058d8a5a86c376b48a9da1975cb47
Vasil Delchev <vasildelchev97@gmail.com>
2024-10-24 17:04:48 +0300

    Added Room comments

--- HotelApp.Infrastructure/Data/Models/Room.cs
+++ HotelApp.Infrastructure/Data/Models/Room.cs
@@ -5,16 +5,20 @@ using static HotelApp.Common.Constants.ValidationConstants;
 
 namespace HotelApp.Infrastructure.Data.Models
 {
+    [Comment("Room in the Hotel")]
     public class Room
     {
         [Key]
+        [Comment("Identifier")]
         public int Id { get; set; }
 
         [Required]
+        [Comment("Type of the room")]
         public RoomType RoomType { get; set; }
 
         [Required]
         [Precision(RequiredPrecision, RequiredScale)]
+        [Comment("Room price for one night")]
         public decimal PricePerNight { get; set; }
 
         public IEnumerable<Reservation> Reservations { get; set; } 
