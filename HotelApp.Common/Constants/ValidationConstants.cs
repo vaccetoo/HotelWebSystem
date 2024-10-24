45fa119ebdc4d4fd871e9fb757b6630dcc81717b
Vasil Delchev <vasildelchev97@gmail.com>
2024-10-24 16:39:15 +0300

    Added validation constants

---
+++ HotelApp.Common/Constants/ValidationConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelApp.Common.Constants
+{
+    public static class ValidationConstants
+    {
+        // Reservation validation constants
+        public const decimal TotalPriceMinValue = 0m;
+        public const decimal TotalPriceMaxValue = 10_000m;
+
+        // Common validation constants
+        public const int RequiredPrecision = 18;
+        public const int RequiredScale = 2;
+    }
+}
