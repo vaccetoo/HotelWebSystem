3fe32e343b4e0c55ad638ff5dde37f75ec5bb0f7
Vasil Delchev <vasildelchev97@gmail.com>
2024-10-24 17:24:47 +0300

    Added Facility comments

--- HotelApp.Infrastructure/Data/Models/Facility.cs
+++ HotelApp.Infrastructure/Data/Models/Facility.cs
@@ -5,26 +5,33 @@ using static HotelApp.Common.Constants.ValidationConstants;
 
 namespace HotelApp.Infrastructure.Data.Models
 {
+    [Comment("Facility in the hotel")]
     public class Facility
     {
         [Key]
+        [Comment("Identifier")]
         public int Id { get; set; }
 
         [Required]
+        [Comment("Type of the facility")]
         public FacilityType FacilityType { get; set; }
 
         [Required]
         [MaxLength(FacilityDescriptionMaxLength)]
+        [Comment("Description of the facility")]
         public string Description { get; set; } = string.Empty;
 
         [Required]
         [Precision(RequiredPrecision, RequiredScale)]
+        [Comment("Price of the facility")]
         public decimal Price { get; set; }
 
         [Required]
+        [Comment("The time current facility will be open at")]
         public DateTime OpenAt { get; set; }
 
         [Required]
+        [Comment("The time current facility will be closed at")]
         public DateTime CloseAt { get; set; }
 
         public IEnumerable<ReservationFacility> ReservationsFacility { get; set; }
