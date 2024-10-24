2e4f1bc6ee691ad7ca637973192c187be2ca39e5
Vasil Delchev <vasildelchev97@gmail.com>
2024-10-24 17:02:13 +0300

    Added Guest comments

--- HotelApp.Infrastructure/Data/Models/Guest.cs
+++ HotelApp.Infrastructure/Data/Models/Guest.cs
@@ -1,23 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using static HotelApp.Common.Constants.ValidationConstants;
 
 namespace HotelApp.Infrastructure.Data.Models
 {
+    [Comment("Guest in the Hotel - (Does not need to be IdentityUser)")]
     public class Guest
     {
         [Key]
+        [Comment("Identifier")]
         public int Id { get; set; }
 
         [Required]
         [MaxLength(GuestFullNameMaxLength)]
+        [Comment("Guest full name")]
         public string FullName { get; set; } = string.Empty;
 
         [Required]
         [RegularExpression(PhoneNumberPattern)]
+        [Comment("Guest phone number")]
         public string PhoneNumber { get; set; } = string.Empty;
 
         [Required]
         [RegularExpression(EmailAddressPattern)]
+        [Comment("Guest email address")]
         public string EmailAddress { get; set; } = string.Empty;
 
         public IEnumerable<ReservationParticipant> ReservationsParticipant { get; set; } 
